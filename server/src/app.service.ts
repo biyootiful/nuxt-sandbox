c8c878fddc1f31be00b6e2dccca01f40cd315ab1
Bi Yoo <yoobi.dev@gmail.com>
2022-08-04 16:14:21 -0400

    add route param

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'OK this is better';
+  getRedirectLink(slug): string {
+    return slug;
   }
 }
