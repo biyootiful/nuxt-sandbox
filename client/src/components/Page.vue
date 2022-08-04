1d6bd40498b655edf1fd1407c550dcc675364ae6
Bi Yoo <yoobi.dev@gmail.com>
2022-08-04 15:33:53 -0400

    add api layer w/ nest

---
+++ client/src/components/Page.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="flex pt-4 justify-center text-center"><h1>Hey there</h1></div>
+</template>
--- components/Page.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div class="flex pt-4 justify-center text-center"><h1>Hey there</h1></div>
-</template>
