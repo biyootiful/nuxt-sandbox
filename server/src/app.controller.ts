0d891aeb2ac7c09fee4d47c4b6c4691a7a18de35
Bi Yoo <yoobi.dev@gmail.com>
2022-08-06 23:34:44 -0400

    Implement redirect logic

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Redirect,
+  Param,
+} from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get(':slug')
-  getRedirectLink(@Param() params): string {
-    return this.appService.getRedirectLink(params.slug);
+  @Redirect('https://localhost:3000', 302)
+  async getRedirectLink(@Param('slug') slug: string): Promise<{ url: string }> {
+    const link = await this.prismaService.link.findFirst({
+      where: { slug: String(slug)}
+    })
+    
+    // to redirect, return,
+    // {
+    //   "url": string,
+    //   "statusCode": number
+    // }
+    // https://docs.nestjs.com/controllers#redirection
+
+    return { url: link.url };
   }
 }
